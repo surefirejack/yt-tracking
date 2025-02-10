6636cfee30f0706c3e3f594e001e64bd9981f50c
Ahmad <swilaaam@gmail.com>
2025-02-10 11:24:03 +0200

    - Show plans on subscription page on dashboard when customer is not subscribed.

--- app/View/Components/Filament/Plans/All.php
+++ app/View/Components/Filament/Plans/All.php
@@ -21,13 +21,14 @@ class All extends \App\View\Components\Plans\All
             $subscription = $this->subscriptionManager->findActiveByUserAndSubscriptionUuid(auth()->user()->id, $this->currentSubscriptionUuid);
         }
 
-        if ($subscription === null) {
-            return [];
+        $planType = null;
+        if ($subscription !== null) {
+            $planType = $subscription->plan->type;
         }
 
         $plans = $this->planManager->getAllPlansWithPrices(
             $this->products,
-            $subscription->plan->type,
+            $planType,
         );
 
         $viewData['subscription'] = $subscription;
