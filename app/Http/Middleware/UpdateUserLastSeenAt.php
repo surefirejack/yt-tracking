cddc9c719ae814e92213cf3f20193bcb2204ea78
Ahmad <swilaaam@gmail.com>
2025-04-30 19:35:20 +0200

    - Add "Last seen at" to know when a user last used your SaaS app. - Update libraries.

--- app/Http/Middleware/UpdateUserLastSeenAt.php
+++ app/Http/Middleware/UpdateUserLastSeenAt.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,6 +18,7 @@ class UpdateUserLastSeenAt
     public function handle(Request $request, Closure $next): Response
     {
         if (Auth::check()) {
+            /** @var User $user */
             $user = Auth::user();
 
             if ($user->last_seen_at === null || now()->diffInMinutes($user->last_seen_at) >= 10) { // not to overload the database
