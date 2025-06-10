e46a7f37d465594eaa320a9ade86a737d52e8812
surefirejack <jackborn@gmail.com>
2025-06-10 10:37:41 +1000

    Spacing between icon and Connect

--- app/Filament/Dashboard/Pages/Settings.php
+++ app/Filament/Dashboard/Pages/Settings.php
@@ -376,7 +376,7 @@ EOT;
                                                            class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
                                                             <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 24 24">
                                                                 <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
-                                                            </svg>
+                                                            </svg> &nbsp;
                                                             Connect YouTube
                                                         </a>
                                                     </div>
